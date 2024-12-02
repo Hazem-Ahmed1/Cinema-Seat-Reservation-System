a445e380ed10bb128cc4f7a4bb5a69df3178c7c7
Hazem Ahmed <111501299+Hazem-Ahmed1@users.noreply.github.com>
2024-12-02 13:03:30 +0200

    Type Structure & File Reader Functions

---
+++ CinemaSeatTypes.fs
@@ -0,0 +1,26 @@
+﻿module CinemaSeatTypes
+open System
+
+// Define a movie type
+type Movie = {
+    Name: string
+    Genre: string
+    Rating: float
+    PosterPath: string
+}
+type Seat = {
+    HallName: string
+    Movie: Movie
+    Class: string
+    Row: int
+    Column: int
+    Available: bool
+    ShowTime : DateTime
+}
+
+// string * string * int * int * bool * DateTime
+
+
+
+type CiemaSeats = Seat List
+
---
+++ FileReader.fs
@@ -0,0 +1,52 @@
+﻿module FileReader
+
+open System.IO
+open CinemaSeatTypes
+open System
+
+let readLinesLazy (filePath: string) : seq<string> =
+    let rec readLines (reader: StreamReader) =
+        seq {
+            if not reader.EndOfStream then
+                yield reader.ReadLine()
+                yield! readLines reader
+        }
+    try
+        seq {
+            use reader = new StreamReader(filePath)
+            yield! readLines reader
+        }
+    with
+    | ex ->
+        printfn "An error occurred: %s" ex.Message
+        Seq.empty
+
+
+
+let parseLineToSeat (line: string) : Seat option =
+    try
+        let parts = line.Split(',')
+        if parts.Length >= 8 then
+            // Parsing Movie data
+            let movieParts = parts.[1].Split('|') // Assuming 'Sci-Fi 8.8' is the movie name and rating
+            let movie = {
+                Name = movieParts.[0] // Name part
+                Genre = movieParts.[1] // Genre part
+                Rating = float movieParts.[2] // Rating part (assuming format like "8.8")
+                PosterPath = movieParts.[3] // Assuming the path is the last column in the string
+            }
+
+            Some {
+                HallName = parts.[0] // Hall Name
+                Movie = movie
+                Class = parts.[2] // Seat Class
+                Row = int parts.[3] // Row
+                Column = int parts.[4] // Column
+                Available = Boolean.Parse(parts.[6]) // Available status
+                ShowTime = DateTime.Parse(parts.[7]) // ShowTime
+            }
+        else None
+    with
+    | ex ->
+        printfn "Failed to parse line: %s. Error: %s" line ex.Message
+        None
